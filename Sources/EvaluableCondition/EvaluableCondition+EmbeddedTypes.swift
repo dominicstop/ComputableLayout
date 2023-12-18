86a68d443882500ca4ba0331efc7f3a25db732a7
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-12-19 03:22:13 +0800

    🛠 Refactor: Extract to `EvaluableConditionSizeValue`
    Summary:
    * Extract `EvaluableCondition.SizeValue` to it's own type/file `EvaluableConditionSizeValue`.

--- Sources/EvaluableCondition/EvaluableCondition+EmbeddedTypes.swift
+++ Sources/EvaluableCondition/EvaluableCondition+EmbeddedTypes.swift
@@ -10,4 +10,5 @@ import Foundation
 public extension EvaluableCondition {
   
   typealias FrameRectValue = EvaluableConditionFrameRectValue;
+  typealias SizeValue = EvaluableConditionSizeValue;
 };
--- Sources/EvaluableCondition/EvaluableCondition.swift
+++ Sources/EvaluableCondition/EvaluableCondition.swift
@@ -15,43 +15,7 @@ public indirect enum EvaluableCondition: Equatable {
   
   
 
-  public enum SizeValue: Equatable {
-    case window;
-    case screen;
-    case statusBar;
-    case targetView;
-    
-    case custom(CGSize);
-    
-    func evaluate(
-      usingContext context: EvaluableConditionContext,
-      forKey key: KeyPath<CGSize, CGFloat>,
-      condition: NumericLogicalExpression<CGFloat>
-    ) -> Bool {
-    
-      let size: CGSize = {
-        switch self {
-          case .window:
-            return context.windowFrame?.size ?? .zero;
-            
-          case .screen:
-            return context.screenBounds.size;
-          
-          case .targetView:
-            return context.targetViewFrame?.size ?? .zero;
-            
-          case .statusBar:
-            return context.statusBarFrame?.size ?? .zero;
-            
-          case let .custom(size):
-            return size;
-        };
-      }();
-      
-      let value = size[keyPath: key];
-      return condition.evaluate(forValue: value);
-    };
-  };
+  
   
   // Mirrors: `UIUserInterfaceStyle`
   public enum UserInterfaceStyle: CaseIterable, Equatable {
